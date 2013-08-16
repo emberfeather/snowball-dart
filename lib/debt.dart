9d57e4038f6e5e2732d02c6536a730cdd5fffb08
Randy Merrill <Zoramite@gmail.com>
2013-08-15 22:28:53 -0700

    Adding more tests for the debts.

--- lib/debt.dart
+++ lib/debt.dart
@@ -4,10 +4,50 @@ library debt;
  * Debt class for describing and working with specific debt information.
  */
 class Debt {
-  var label;
-  num balance;
-  num rate;
-  num minPayment;
+  var _label;
+  num _balance;
+  num _rate;
+  num _minPayment;
   
-  Debt(this.label, this.balance, this.rate, this.minPayment);
+  Debt(label, balance, rate, minPayment) {
+    this.label = label;
+    this.balance = balance;
+    this.rate = rate;
+    this.minPayment = minPayment;
+  }
+  
+  get balance => _balance;
+  set balance(num value) {
+    if (value.isNegative) {
+      throw new Exception('Balance cannot be negative');
+    }
+    _balance = value;
+  }
+  
+  get label => _label;
+  set label(var value) {
+    if (value.isEmpty) {
+      throw new Exception('Label cannot be empty');
+    }
+    _label = value;
+  }
+  
+  get minPayment => _minPayment;
+  set minPayment(num value) {
+    if (value.isNegative) {
+      throw new Exception('Minimum payment cannot be negative');
+    }
+    _minPayment = value;
+  }
+  
+  get rate => _rate;
+  set rate(num value) {
+    if (value.isNegative) {
+      throw new Exception('Rate cannot be negative');
+    }
+    if (value > 3) {
+      throw new Exception('Rate cannot be over 3');
+    }
+    _rate = value;
+  }
 }
--- test/debt_test.dart
+++ test/debt_test.dart
@@ -10,11 +10,79 @@ void setup() {
 }
 
 main() {
-  test('simple values', () {
+  test('initial values', () {
     setup();
     expect(debt1.label, 'Test1');
     expect(debt1.balance, 2500);
     expect(debt1.rate, .1);
     expect(debt1.minPayment, 25);
+  });
+  
+  group('label', () {
+    test('reassignment', () {
+      setup();
+      expect(debt1.label, 'Test1');
+      debt1.label = 'Test1.0';
+      expect(debt1.label, 'Test1.0');
+    });
+    
+    test('blank', () {
+      setup();
+      expect(debt1.label, 'Test1');
+      expect(() { debt1.label = ''; }, throwsException);
+    });
+  });
+  
+  group('balance', () {
+    test('reassignment', () {
+      setup();
+      expect(debt1.balance, 2500);
+      debt1.balance = 1000;
+      expect(debt1.balance, 1000);
+    });
+    
+    test('negative', () {
+      setup();
+      expect(() { debt1.balance = -1; }, throwsException);
+    });
+  });
+  
+  group('rate', () {
+    test('reassignment', () {
+      setup();
+      expect(debt1.rate, .1);
+      debt1.rate = .23;
+      expect(debt1.rate, .23);
+    });
+    
+    test('negative', () {
+      setup();
+      expect(() { debt1.rate = -.1; }, throwsException);
+    });
+    
+    test('too large', () {
+      setup();
+      expect(() { debt1.rate = 3.1; }, throwsException);
+    });
+  });
+  
+  group('min payment', () {
+    test('reassignment', () {
+      setup();
+      expect(debt1.minPayment, 25);
+      debt1.minPayment = 135;
+      expect(debt1.minPayment, 135);
+    });
+    
+    test('negative', () {
+      setup();
+      expect(() { debt1.minPayment = -.1; }, throwsException);
+    });
+    
+    test('zero', () {
+      setup();
+      debt1.minPayment = 0;
+      expect(debt1.minPayment, 0);
+    });
   });
 }
