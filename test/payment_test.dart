3640d326b09dd439317a5584d63ea137db9a4675
Randy Merrill <Zoramite@gmail.com>
2013-12-27 10:20:13 -0800

    Adding tests for the payment class.

---
+++ test/payment_test.dart
@@ -0,0 +1,20 @@
+library test.payment_test;
+
+import 'package:unittest/unittest.dart';
+import 'package:snowball/payment.dart';
+
+main() {
+  test('Simple values', () {
+    var payment = new Payment(25.0, 5.0);
+    expect(payment.principal, 25.0);
+    expect(payment.interest, 5.0);
+    expect(payment.total, 30.0);
+  });
+
+  test('Optional interest', () {
+    var payment = new Payment(25.0);
+    expect(payment.principal, 25.0);
+    expect(payment.interest, 0.0);
+    expect(payment.total, 25.0);
+  });
+}
