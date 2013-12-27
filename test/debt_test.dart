9730af6a211d5ad23c1d21454a2579cbd98ae80b
Randy Merrill <Zoramite@gmail.com>
2013-12-26 21:36:44 -0800

    Updating for changes to dart.

--- test/debt_test.dart
+++ test/debt_test.dart
@@ -9,8 +9,8 @@ void setup() {
   debt1 = new Debt('Test1', 2500, .1, 25);
 }
 
-main() {
-  test('initial values', () {
+void main() {
+  test('Initial values', () {
     setup();
     expect(debt1.label, 'Test1');
     expect(debt1.balance, 2500);
@@ -18,68 +18,68 @@ main() {
     expect(debt1.minPayment, 25);
   });
   
-  group('label', () {
-    test('reassignment', () {
+  group('Label', () {
+    test('Reassignment', () {
       setup();
       expect(debt1.label, 'Test1');
       debt1.label = 'Test1.0';
       expect(debt1.label, 'Test1.0');
     });
     
-    test('blank', () {
+    test('Blank', () {
       setup();
       expect(debt1.label, 'Test1');
       expect(() { debt1.label = ''; }, throwsException);
     });
   });
   
-  group('balance', () {
-    test('reassignment', () {
+  group('Balance', () {
+    test('Reassignment', () {
       setup();
       expect(debt1.balance, 2500);
       debt1.balance = 1000;
       expect(debt1.balance, 1000);
     });
     
-    test('negative', () {
+    test('Negative', () {
       setup();
       expect(() { debt1.balance = -1; }, throwsException);
     });
   });
   
-  group('rate', () {
-    test('reassignment', () {
+  group('Rate', () {
+    test('Reassignment', () {
       setup();
       expect(debt1.rate, .1);
       debt1.rate = .23;
       expect(debt1.rate, .23);
     });
     
-    test('negative', () {
+    test('Negative', () {
       setup();
       expect(() { debt1.rate = -.1; }, throwsException);
     });
     
-    test('too large', () {
+    test('Too large', () {
       setup();
       expect(() { debt1.rate = 3.1; }, throwsException);
     });
   });
   
-  group('min payment', () {
-    test('reassignment', () {
+  group('Min payment', () {
+    test('Reassignment', () {
       setup();
       expect(debt1.minPayment, 25);
       debt1.minPayment = 135;
       expect(debt1.minPayment, 135);
     });
     
-    test('negative', () {
+    test('Negative', () {
       setup();
       expect(() { debt1.minPayment = -.1; }, throwsException);
     });
     
-    test('zero', () {
+    test('Zero', () {
       setup();
       debt1.minPayment = 0;
       expect(debt1.minPayment, 0);
--- web/snowball.dart
+++ web/snowball.dart
@@ -1,16 +1,16 @@
 import 'dart:html';
 
 void main() {
-  query("#sample_text_id")
+  document.querySelector("#sample_text_id")
     ..text = "Click me!"
     ..onClick.listen(reverseText);
 }
 
 void reverseText(MouseEvent event) {
-  var text = query("#sample_text_id").text;
+  var text = document.querySelector("#sample_text_id").text;
   var buffer = new StringBuffer();
   for (int i = text.length - 1; i >= 0; i--) {
     buffer.write(text[i]);
   }
-  query("#sample_text_id").text = buffer.toString();
+  document.querySelector("#sample_text_id").text = buffer.toString();
 }
