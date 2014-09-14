5ee57bc172b65d69300233906913917f321b8823
Randy Merrill <Zoramite@gmail.com>
2014-09-14 12:00:31 -0700

    Fixing the broken tests from the change to check for min payment minimum.

--- test/snowballer_test.dart
+++ test/snowballer_test.dart
@@ -9,10 +9,10 @@ void setup() {
   // Map of debt arrays for different test situations.
   debts = {
     'eqBalanceAscRateEqMinimum': [
-      new Debt('Test1', 2500.0, .11, 25.0),
-      new Debt('Test2', 2500.0, .12, 25.0),
-      new Debt('Test3', 2500.0, .13, 25.0),
-      new Debt('Test4', 2500.0, .14, 25.0)
+      new Debt('Test1', 2500.0, .11, 40.0),
+      new Debt('Test2', 2500.0, .12, 40.0),
+      new Debt('Test3', 2500.0, .13, 40.0),
+      new Debt('Test4', 2500.0, .14, 40.0)
     ]
   };
 }
@@ -29,7 +29,6 @@ main() {
     test('Highest Balance First', () {
       setup();
       var snowballer = new Snowballer([HIGHESTBALANCEFIRST]);
-
     });
   });
 }
